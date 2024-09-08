abb180e39677987d828d782c13e12e0141c48206
Jeong Hoon <gbg9609@naver.com>
2024-09-08 11:54:59 +0900

    first commit

---
+++ src/redux/modules/todos.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState: {
+    todos: [
+      {
+        id: new Date().getTime(),
+        title: "react를 배워봅시다.",
+        isDone: false,
+      },
+    ],
+  },
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos = [...state.todos, action.payload];
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload.id);
+    },
+    toggleTodo: (state, action) => {
+      state.todos = state.todos.map((todo) =>
+        todo.id === action.payload.id ? { ...todo, isDone: !todo.isDone } : todo
+      );
+    },
+  },
+});
+
+export const { addTodo, deleteTodo, toggleTodo } = todosSlice.actions;
+export default todosSlice.reducer;
